313653f4df294760c0037844a4cbfde1c2f4d97d
Satyam Yadav <satyamyadavs697@gmail.com>
2025-05-08 09:23:37 +0530

    Update config.py
    Signed-off-by: Satyam Yadav  <satyamyadavs697@gmail.com>

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousX1025/AnonXMusic",
+    "https://github.com/satyamyadav697/AnonXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
