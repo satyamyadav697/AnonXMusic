27f1786a5e20fbe4652f0b4c43e9bdfc921aa4a8
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 <duhgfhfdghg+A1@gmail.com>
2025-05-20 22:26:33 +0530

    Update config.py
    Signed-off-by: 𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 <duhgfhfdghg+A1@gmail.com>

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/satyamyadav697/AnonXMusic",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
